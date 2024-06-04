f2892c755db29aceb560b0758670cfac7a32d468
Damon <ddiazny919@gmail.com>
2024-06-04 05:58:20 -0400

    added responsivness to hero.js text

--- frontend/src/components/services/dataExtraction/Hero.js
+++ frontend/src/components/services/dataExtraction/Hero.js
@@ -28,15 +28,15 @@ const Hero = ({
       <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center px-[10px]'>
         <p className='text-[#fff] font-bold p-2'>
         </p>
-        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>
+        <h1 className='md:text-7xl sm:text-4xl text-2xl font-bold md:py-6'>
           Custom Web Scraping Services
         </h1>
         <div className='flex justify-center items-center'>
-          <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>
+          <p className='md:text-5xl sm:text-xl text-xl font-bold py-4'>
             
           </p>
         </div>
-        <p className='text-lg font-bold text-slate-300'>Effortlessly obtain clean, structured data from the web without the complexities of developing or maintaining your own scrapers. Let us handle the hard work, so you can focus on making data-driven decisions.</p>
+        <p className='md:text-lg text-md font-bold text-slate-300'>Effortlessly obtain clean, structured data from the web without the complexities of developing or maintaining your own scrapers. Let us handle the hard work, so you can focus on making data-driven decisions.</p>
         <button onClick={scrollToComponent} className='flex flex-row bg-[#715df2] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-white justify-center items-center hover:bg-[#8B7AF4]'>
           <span>Request Quote</span>
         </button>
